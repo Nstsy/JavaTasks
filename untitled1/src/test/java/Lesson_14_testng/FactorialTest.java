01f3dc585e5c2958e9266e65cbc907251028ad2b
nstsy <nstsy@yandex.ru>
2024-10-20 19:52:41 +0300

    add Lesson_14_testNG

---
+++ untitled1/src/test/java/Lesson_14_testng/FactorialTest.java
@@ -0,0 +1,29 @@
+package Lesson_14_testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FactorialTest {
+
+    Factorial factorial = new Factorial();
+
+    @Test
+    public void testFactorialPositive() {
+        Assert.assertEquals(120, factorial.getFactorial(5), "Факториал 5 должен быть 120");
+    }
+
+    @Test
+    public void testFactorialZero() {
+        Assert.assertEquals(1, factorial.getFactorial(1), "Факториал 0 должен быть 1");
+    }
+
+    @Test
+    public void testFactorialBig() {
+        Assert.assertEquals(3628800, factorial.getFactorial(10), "Факториал 10 должен быть 3628800");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialNegative() {
+        factorial.getFactorial(-1);
+    }
+}
